43ca883acd034d67bc96bb8ae40742694c7c3f54
liubo <liubo@topjoy.com>
2021-03-26 20:39:17 +0800

    Initial Commit

---
+++ LeanOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeanOpenGL
+//
+//  Created by 刘波 on 2021/3/26.
+//  Copyright © 2021 刘波. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
