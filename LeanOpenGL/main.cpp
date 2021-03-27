4a1a0af08346be6c7d59e028f388c63ca4a626c0
liubo <liubo@topjoy.com>
2021-03-27 19:45:22 +0800

    init project1

--- LeanOpenGL/main.cpp
+++ LeanOpenGL/main.cpp
@@ -7,9 +7,15 @@
 //
 
 #include <iostream>
+#include <glad/glad.h>
+#include "GLFW/glfw3.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
     return 0;
 }
